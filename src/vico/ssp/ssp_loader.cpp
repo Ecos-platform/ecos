5d9d864d197436942ed9c7c289c79ba6eafbe099
Lars Ivar Hatledal <larsivarhatledal@gmail.com>
2022-07-02 21:49:12 +0200

    update

--- src/vico/ssp/ssp_loader.cpp
+++ src/vico/ssp/ssp_loader.cpp
@@ -1,14 +1,14 @@
 
 #include "vico/ssp/ssp_loader.hpp"
 
-#include <ssp/ssp_parser.hpp>
+#include <ssp/ssp.hpp>
 
 using namespace vico;
 
 
 simulation_structure vico::load_ssp(const fs::path& path)
 {
-    ssp::SystemStructureDescription desc(path.string());
+    ssp::SystemStructureDescription desc(path);
 
     return {};
 }

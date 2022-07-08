a9dc31143fb09f8b83c048fd7c27ae5a6ce8fa64
Lars Ivar Hatledal <larsivarhatledal@gmail.com>
2022-07-08 09:49:22 +0200

    refactor

--- src/vico/util/uuid.hpp
+++ src/vico/util/uuid.hpp
@@ -2,46 +2,12 @@
 #ifndef VICO_UUID_HPP
 #define VICO_UUID_HPP
 
-#include <ios>
-#include <random>
-#include <sstream>
 #include <string>
 
 namespace vico
 {
 
-std::string generate_uuid()
-{
-    static std::random_device rd;
-    static std::mt19937 gen(rd());
-    static std::uniform_int_distribution<> dis(0, 15);
-    static std::uniform_int_distribution<> dis2(8, 11);
-
-    int i;
-    std::stringstream ss;
-    ss << std::hex;
-    for (i = 0; i < 8; i++) {
-        ss << dis(gen);
-    }
-    ss << "-";
-    for (i = 0; i < 4; i++) {
-        ss << dis(gen);
-    }
-    ss << "-4";
-    for (i = 0; i < 3; i++) {
-        ss << dis(gen);
-    }
-    ss << "-";
-    ss << dis2(gen);
-    for (i = 0; i < 3; i++) {
-        ss << dis(gen);
-    }
-    ss << "-";
-    for (i = 0; i < 12; i++) {
-        ss << dis(gen);
-    }
-    return ss.str();
-}
+std::string generate_uuid();
 
 } // namespace vico
 

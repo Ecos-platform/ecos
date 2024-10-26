bc5f788ea0d4983c995785eb4e202a4574ee26b3
Lars Ivar Hatledal <larsivarhatledal@gmail.com>
2024-10-27 00:28:52 +0200

    add python example

---
+++ examples/controlled_temperature/controlled_temperature.py
@@ -0,0 +1,23 @@
+from ecospy import *
+
+
+def main():
+    print(f"Ecoslib version: {EcosLib.version()}")
+
+    EcosLib.set_log_level("debug")
+
+    fmu_path = f"{__file__}/../../../data/fmus/2.0/20sim/ControlledTemperature.fmu"
+
+    ss = EcosSimulationStructure()
+    if not ss.add_model("instance", fmu_path):
+        print(EcosLib.get_last_error())
+
+    sim = EcosSimulation(structure=ss, step_size=1/100)
+    sim.add_csv_writer("results/python/temperature.csv")
+    sim.init()
+    sim.step_until(10)
+    sim.terminate()
+
+
+if __name__ == "__main__":
+    main()

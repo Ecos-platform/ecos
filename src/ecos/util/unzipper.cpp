dec08f1cbc3d48d2958799c2126433efe93b80ee
Lars Ivar Hatledal <larsivarhatledal@gmail.com>
2024-10-31 20:54:38 +0100

    remove libzip dep (#19)

--- src/ecos/util/unzipper.cpp
+++ src/ecos/util/unzipper.cpp
@@ -1,59 +1,30 @@
 
 #include "unzipper.hpp"
 
-#include "zip.h"
-
 #include <fstream>
+#include <string>
 #include <vector>
-
 
 bool ecos::unzip(const std::filesystem::path& zip_file, const std::filesystem::path& tmp_path)
 {
-    int* err = nullptr;
-    zip* za = zip_open(absolute(zip_file).string().c_str(), 0, err);
-    if (za == nullptr) {
+
+    if (!exists(zip_file)) {
         return false;
     }
 
-    struct zip_file* zf;
-    // clang-format off
-    struct zip_stat sb{};
-    // clang-format on
+#ifdef _WIN32
+    // Windows: Use the built-in tar command
+    std::string command = "tar -xf \"" + zip_file.string() + "\" -C \"" + tmp_path.string() + "\"";
+#else
+    // Linux: Use the unzip command and supress output
+    std::string command = "unzip -o \"" + zip_file.string() + "\" -d \"" + tmp_path.string() + "\" > /dev/null 2>&1";
+#endif
 
-    constexpr int bufferSize = 1000;
-    std::vector<char> contents(bufferSize);
-    for (int i = 0; i < zip_get_num_entries(za, 0); i++) {
-        if (zip_stat_index(za, i, 0, &sb) == 0) {
+    const int result = std::system(command.c_str());
 
-            const std::filesystem::path newFile = tmp_path / sb.name;
-
-            if (sb.size == 0) {
-                create_directories(newFile);
-            } else {
-                const auto containingDirectory = newFile.parent_path();
-                if (!exists(containingDirectory) && !create_directories(containingDirectory)) {
-                    return false;
-                }
-                zf = zip_fopen_index(za, i, 0);
-
-                std::ofstream file;
-                file.open(newFile, std::ios::out | std::ios::binary);
-
-                zip_int64_t sum = 0;
-                while (sum != sb.size) {
-                    zip_int64_t len = zip_fread(zf, contents.data(), bufferSize);
-                    file.write(contents.data(), len);
-                    sum += len;
-                }
-
-                file.flush();
-                file.close();
-
-                zip_fclose(zf);
-            }
-        }
+    if (result != 0) {
+        return false;
     }
-    zip_close(za);
 
     return true;
 }

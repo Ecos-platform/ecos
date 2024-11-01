f72415aad9eb2052b0e8568e95abdf36fcee0576
Lars Ivar Hatledal <larsivarhatledal@gmail.com>
2024-11-01 12:47:13 +0100

    add spring-mass-damper python demo

--- ecospy/EcosSimulationStructure.py
+++ ecospy/EcosSimulationStructure.py
@@ -13,12 +13,37 @@ class EcosSimulationStructure:
         self._add_model.argtypes = [c_void_p, c_char_p, c_char_p]
         self._add_model.restype = c_bool
 
+        self._make_int_connection = dll.ecos_simulation_structure_make_int_connection
+        self._make_int_connection.argtypes = [c_void_p, c_char_p, c_char_p]
+
+        self._make_real_connection = dll.ecos_simulation_structure_make_real_connection
+        self._make_real_connection.argtypes = [c_void_p, c_char_p, c_char_p]
+
+        self._make_string_connection = dll.ecos_simulation_structure_make_string_connection
+        self._make_string_connection.argtypes = [c_void_p, c_char_p, c_char_p]
+
+        self._make_bool_connection = dll.ecos_simulation_structure_make_bool_connection
+        self._make_bool_connection.argtypes = [c_void_p, c_char_p, c_char_p]
+
+
         self.handle = create_simulation_structure()
         if self.handle is None:
             raise Exception(EcosLib.get_last_error())
 
     def add_model(self, instance_name: str, uri: str) -> bool:
         return self._add_model(self.handle, instance_name.encode(), uri.encode())
+
+    def make_real_connection(self, source: str, sink: str):
+        self._make_real_connection(self.handle, source.encode(), sink.encode())
+
+    def make_int_connection(self, source: str, sink: str):
+        self._make_int_connection(self.handle, source.encode(), sink.encode())
+
+    def make_string_connection(self, source: str, sink: str):
+        self._make_string_connection(self.handle, source.encode(), sink.encode())
+
+    def make_bool_connection(self, source: str, sink: str):
+        self._make_bool_connection(self.handle, source.encode(), sink.encode())
 
     def __del__(self):
         simulation_structure_destroy = dll.ecos_simulation_structure_destroy
--- examples/spring_mass_damper/spring_mass_damper.py
+++ examples/spring_mass_damper/spring_mass_damper.py
@@ -1,21 +1,39 @@
 
 from ecospy import *
+from ecospy.plotter import *
 
 def main():
 
     fmuFolder = f"{__file__}/../../../data/fmus/1.0"
+    result_file = f"{__file__}/../results/python/bouncing_ball.csv"
 
     ss = EcosSimulationStructure()
     ss.add_model("damper", f"proxyfmu://localhost?file={fmuFolder}/Damper.fmu")
     ss.add_model("mass", f"proxyfmu://localhost?file={fmuFolder}/Mass.fmu")
     ss.add_model("spring", f"proxyfmu://localhost?file={fmuFolder}/Spring.fmu")
 
-
-
+    ss.make_real_connection("spring::for_xx", "mass::in_l_u")
+    ss.make_real_connection("spring::for_yx", "mass::in_l_w")
+    ss.make_real_connection("mass::out_l_u", "spring::dis_xx")
+    ss.make_real_connection("mass::out_l_w", "spring::dis_yx")
+    ss.make_real_connection("damper::df_0", "mass::in_f_u")
+    ss.make_real_connection("damper::df_1", "mass::in_f_w")
+    ss.make_real_connection("mass::out_f_u", "damper::lv_0")
+    ss.make_real_connection("mass::out_f_w", "damper::lv_1")
+    
     sim = EcosSimulation(structure=ss, step_size=1.0/100)
+    sim.add_csv_writer(result_file)
+
     sim.init()
-    sim.step_until(5)
+    sim.step_until(80)
     sim.terminate()
+
+    config = TimeSeriesConfig(
+        title="Mass-spring-damper",
+        y_label="Height[m]",
+        identifiers=["mass::out_f_w"])
+    plotter = Plotter(result_file, config)
+    plotter.show()
 
 
 if __name__ == "__main__":

ddf101fd1c4995ddce3cc35a11b1e1ff07ec0ca3
Lars Ivar Hatledal <larsivarhatledal@gmail.com>
2025-03-13 00:16:00 +0100

    update

--- ecospy/setup.py
+++ ecospy/setup.py
@@ -1,10 +1,21 @@
-
+import os
 from setuptools import setup
+
+WINDOWS = (os.name == 'nt')
+
 
 def version():
     with open("../version.txt", "r") as f:
         return f.readline().strip()
 
+
+def binary_suffix():
+    return ".exe" if WINDOWS else ""
+
+
 setup(name="ecospy",
-      version=version()
+      version=version(),
+      data_files=[
+          ("Scripts", [f"binaries/proxyfmu{binary_suffix()}", f"binaries/ecos{binary_suffix()}"])
+      ]
       )

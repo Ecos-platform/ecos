3a1e2dbbe1f105a89aea5630ca29df1b11508de8
Lars Ivar Hatledal <larsivarhatledal@gmail.com>
2025-04-11 20:31:26 +0200

    make utils header-only

--- src/ecos/util/temp_dir.hpp
+++ src/ecos/util/temp_dir.hpp
@@ -2,6 +2,9 @@
 #ifndef ECOS_TEMP_DIR_HPP
 #define ECOS_TEMP_DIR_HPP
 
+#include "ecos/logger/logger.hpp"
+#include "ecos/util/uuid.hpp"
+
 #include <filesystem>
 
 namespace ecos
@@ -11,7 +14,11 @@ class temp_dir
 {
 
 public:
-    explicit temp_dir(const std::string& name);
+    explicit temp_dir(const std::string& name)
+    : path_(safe_temp_directory_path() /= "ecos_" + name + "_" + generate_uuid())
+    {
+        create_directories(path_);
+    }
 
     temp_dir(temp_dir&&) = delete;
     temp_dir(const temp_dir&) = delete;
@@ -24,10 +31,27 @@ public:
         return path_;
     }
 
-    ~temp_dir();
+    ~temp_dir()
+    {
+        std::error_code status;
+        remove_all(path_, status);
+        if (status) {
+            log::warn("Failed to remove temp folder '{}': {}", path_.string(), status.message());
+        }
+    }
 
 private:
     const std::filesystem::path path_;
+
+    static std::filesystem::path safe_temp_directory_path() {
+        try {
+            return std::filesystem::temp_directory_path(); // fails in debug under WSL
+        } catch (const std::filesystem::filesystem_error& e) {
+            log::warn("temp_directory_path() failed: {}", e.what());
+            // Fallback to /tmp (standard on Unix)
+            return "/tmp";
+        }
+    }
 };
 
 } // namespace ecos
--- src/ecos/util/uuid.hpp
+++ src/ecos/util/uuid.hpp
@@ -2,12 +2,46 @@
 #ifndef ECOS_UUID_HPP
 #define ECOS_UUID_HPP
 
+#include <ios>
+#include <random>
+#include <sstream>
 #include <string>
 
 namespace ecos
 {
 
-std::string generate_uuid();
+inline std::string generate_uuid()
+{
+    static std::random_device rd;
+    static std::mt19937 gen(rd());
+    static std::uniform_int_distribution<> dis(0, 15);
+    static std::uniform_int_distribution<> dis2(8, 11);
+
+    int i;
+    std::stringstream ss;
+    ss << std::hex;
+    for (i = 0; i < 8; i++) {
+        ss << dis(gen);
+    }
+    ss << "-";
+    for (i = 0; i < 4; i++) {
+        ss << dis(gen);
+    }
+    ss << "-4";
+    for (i = 0; i < 3; i++) {
+        ss << dis(gen);
+    }
+    ss << "-";
+    ss << dis2(gen);
+    for (i = 0; i < 3; i++) {
+        ss << dis(gen);
+    }
+    ss << "-";
+    for (i = 0; i < 12; i++) {
+        ss << dis(gen);
+    }
+    return ss.str();
+}
 
 } // namespace ecos
 
